c42b5e2e00b21f804bb2bc83916b90aa27609798
Rose Ulldemolins <a9854261@9854261s-MacBook-Pro.local>
2019-04-01 14:51:24 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// Data types & Variables
+var myName = "Rose";
+var mySurname = "Ulldemolins";
+var myAge = 24;
+console.log(myName + " " + mySurname);
+// Conditionals
+var pinNumber = 1234;
+if(pinNumber === 1234){
+  console.log("Correct pin number");
+}else{
+  console.log("Incorrect pin number");
+}
+// Loops
+var animals = ['cat','donkey','goose'];
+for (var i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
+}
+// Functions
+var printName = function(){
+  console.log("Rose");
+}
+printName();
+printName();
+printName();
+printName();
+// Arrays
+var pubNames = ['Cloisters', '4042' , 'The Red Lion'];
+console.log(pubNames);
+// Objects
+var person = {
+  name: "Rose",
+  pinNumber: 1234,
+  tel: 07772295891
+}
+console.log(person);
+// comment
